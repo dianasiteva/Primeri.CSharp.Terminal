1fd7ab55ad03fad0b933b107267db8533a74225c
Diana Siteva <dianasiteva@hotmail.com>
2016-03-22 15:12:50 +0200

    Модул04, Видео01 - цикъл for

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[] _table = { "Иван", "Гошо", "Гергана" };
+			//Антетка на таблицата
+			Console.WriteLine ("Име");
+			Console.WriteLine ("=========");
+			//Тяло на таблицата
+			for (int i = 0; i < _table.Length; i++) {
+				//Код на цикъла
+				Console.WriteLine ( _table[i]);
+			}
+
+		}
+	}
+}
