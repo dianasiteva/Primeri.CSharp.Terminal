74089754fdb43868716ff92fb804267ff9d99ece
Diana Siteva <dianasiteva@hotmail.com>
2016-03-22 16:15:06 +0200

    Модул04, Видео01 - цикъл for, влагане на цикли

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -6,16 +6,81 @@ namespace Modul04
 	{
 		public static void Main (string[] args)
 		{
-			string[] _table = { "Иван", "Гошо", "Гергана" };
+			string[,] _table = {
+				{"Иван",	 "Георгиев",	 "25" },
+				{"Цветан",   "Цветанов",     "45" },
+				{"Бойко",	 "Борисов", 	 "57" },
+				{"Ивана",	 "Тръмп",	     "40" },
+				{"Аджелина", "Джоли", 	     "45" },
+				{"Брад", 	 "Пит",	         "46" }
+			};
 			//Антетка на таблицата
-			Console.WriteLine ("Име");
-			Console.WriteLine ("=========");
+			Console.WriteLine ("Име".PadRight(12) + "Фамилия".PadRight(12) + "Години".PadRight(12));
+			Console.WriteLine ("====================================");
 			//Тяло на таблицата
-			for (int i = 0; i < _table.Length; i++) {
-				//Код на цикъла
-				Console.WriteLine ( _table[i]);
+			for (int ii = 0; ii < _table.Length/3; ii++) {
+				for (int jj=0; jj<3; jj++){
+					Console.Write (_table[ii,jj].PadRight(12));
+					
+				}
+
+				Console.WriteLine ( );
 			}
 
+
+
+			//Задачата
+
+			int[] a = {1,2,3};
+			int i=0, j=0;
+			int r_op=0; // Тук записваме произволната команда
+			int r_num1=0; // Номер на първо произволно число от масива
+			int r_num2=0; // Номер на второ произволно число от масива
+			var r = new Random();
+
+			string rs = ""; // Тук ще пазим текстовата формула до момента
+			int res = 0; // Тук ще смятаме произволно
+			int found = 0; // Тук пазим колко формули сме намерили до момента
+
+			for(i=0; i<100;i++) {
+				rs = ""; // Всяка нова итерация ще изтриваме rs и res 
+				res = 0; 
+				for(j=0; j<20; j++) {
+					r_op = r.Next(0,3); // Тук се решава дали ще събираме, изваждаме или делим
+					r_num1 = r.Next(0,3);
+					r_num2 = r.Next(0,3);
+
+					switch(r_op) {
+					case 0: 
+						{
+							res += a[r_num1] + a[r_num2];
+							rs += a[r_num1]+" + "+a[r_num2]+ " + ";
+							break;      
+						}
+					case 1: 
+						{
+							res += a[r_num1] * a[r_num2];
+							rs += a[r_num1]+" * "+a[r_num2]+ " + ";
+							break;      
+						}
+					case 2: 
+						{
+							res += a[r_num1] - a[r_num2];
+							rs += a[r_num1]+" - "+a[r_num2]+ " + ";
+							break;      
+						}
+					}
+
+					if (res == 15) { // Воала. Намерихме правилна формула
+						Console.WriteLine(rs+ " 0 = 15\n");
+						found++;
+						break;
+					}
+
+				}
+				if (found > 5) break; // Не искаме повече от 5 формули
+
+			}
 		}
 	}
 }
