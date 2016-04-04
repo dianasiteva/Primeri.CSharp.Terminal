aabf90dc558f68872312713405e61c36e3b1fdc7
Diana Siteva <dianasiteva@hotmail.com>
2016-04-04 16:51:16 +0300

    Модул 05, Видео 01 - методи

---
+++ Modul05/Modul05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Modul05
+{
+	class MainClass
+	{
+		public static void Main (string[] args)   
+		{
+			//Дефиниции
+			string usercommand = "";
+
+
+			//Потребителски команди
+			do {
+				Console.Write ("$ "); usercommand = Console.ReadLine();
+				if ( usercommand.Contains ( "test" )  ) test ( usercommand );
+
+			} while (usercommand != "exit");
+
+
+
+		}
+		//Действие
+		public static void test ( string _input )           //test 5 -> 5*2
+		{
+			try{
+					double _temp=0;
+				if ( double.TryParse ( _input.Split (' ')[1], out _temp ) )
+					{
+						Console.WriteLine( "\nРезултатът от " + _temp.ToString() + "*2 e " + (_temp*_temp).ToString() );
+					}
+
+			}catch{ 
+				}
+
+		}
+
+
+		//Да върне параметри
+
+
+
+
+	}
+}
