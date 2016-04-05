d77a07893a940fb5fb0930e38d0f39166ee62687
Diana Siteva <dianasiteva@hotmail.com>
2016-04-05 11:29:57 +0300

    Модул 05, Видео 01 - методи

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -13,15 +13,15 @@ namespace Modul05
 			//Потребителски команди
 			do {
 				Console.Write ("$ "); usercommand = Console.ReadLine();
-				if ( usercommand.Contains ( "test" )  ) test ( usercommand );
-
+				if ( usercommand.Contains ( "cmd01" )  ) cmd01 ( usercommand );
+				if ( usercommand.Contains ( "cmd02" )  ) Console.WriteLine ( cmd02 ( usercommand ));
 			} while (usercommand != "exit");
 
 
 
 		}
 		//Действие
-		public static void test ( string _input )           //test 5 -> 5*2
+		public static void cmd01 ( string _input )           //cmd01 5 -> 5*2
 		{
 			try{
 					double _temp=0;
@@ -37,9 +37,54 @@ namespace Modul05
 
 
 		//Да върне параметри
+		public static int cmd02 ( string _input )               //cmd02
+		{
+			int _temp = 0;
 
+				if ( int.TryParse ( _input.Split (' ')[1], out _temp ) )
+				{
+					_temp = _temp * _temp * _temp;
+
+				}
+				return _temp;
+
+
+		}
 
 
 
 	}
 }
+//using System;
+//using System.Collections.Generic;
+//
+//namespace TestovaPrograma {
+//	class MainClass {
+//		public static void Main (string[] args) {
+//
+//			// Изпълнете функцията тук           
+//			test("Моето съобщение");
+//		}
+//		// Добавете новата функция тук
+//		public static void test( string msg ) {
+//			Console.WriteLine( msg );
+//		}
+//	}
+//}
+
+//using System;
+//using System.Collections.Generic;
+//
+//namespace TestovaPrograma {
+//	class MainClass {
+//		public static void Main (string[] args) {
+//
+//			// Изпълнете функцията тук           
+//			int proba = suma( 5, 11); Console.WriteLine(proba);
+//		}
+//		// Добавете новата функция тук
+//		public static int suma( int a, int b) {
+//			return a + b;
+//		}
+//	}
+//}
